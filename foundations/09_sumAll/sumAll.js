e0bff3aee1fc6d2b95db319dbd0fb7c4a5fd5c57
nilikopo <nilikopo9@gmail.com>
2026-02-19 09:30:11 +1000

    09_sumall first solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(startVal, endVal) {
+  let sum = 0
+  for (let i = startVal; i <= endVal; i++) {
+    sum += i
+  }
+  return sum
 };
 
 // Do not edit below this line

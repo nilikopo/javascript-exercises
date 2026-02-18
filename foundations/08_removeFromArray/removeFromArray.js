c0254570599abfc9b251345fa7b2a04e4175c87c
nilikopo <nilikopo9@gmail.com>
2026-02-19 09:07:11 +1000

    08_removefromarray first solution

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, item) {
+  const index = arr.indexOf(item)
+  const removedElement = arr.splice( index, 1 )
+  return arr
 };
 
 // Do not edit below this line

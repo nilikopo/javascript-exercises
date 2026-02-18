09758d78c128eb77d6e1ff9d8ccc578143a6f325
nilikopo <nilikopo9@gmail.com>
2026-02-19 08:42:56 +1000

    06_repeatstring solution

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, amount) {
+  if (amount < 0) return 'ERROR'
+  let result = ''
+  for (let i = 0; i < amount; i++) {
+    result += str
+  }
+  return result
 };
 
 // Do not edit below this line

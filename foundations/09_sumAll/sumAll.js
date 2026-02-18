4ba3ed0c0ef2ac60bf74f13236a8cb22443e1f99
nilikopo <nilikopo9@gmail.com>
2026-02-19 09:38:20 +1000

    09_sumall final solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
 const sumAll = function(startVal, endVal) {
   let sum = 0
-  for (let i = startVal; i <= endVal; i++) {
+  const min = startVal < endVal ? startVal : endVal
+  const max = startVal > endVal ? startVal : endVal
+
+  if (min < 0 || max < 0) {
+    return 'ERROR'
+  } else if (typeof min !== "number" || typeof max !== "number") {
+    return 'ERROR'
+  } else if (Math.floor(min) !== min || Math.floor(max) !== max) {
+    return 'ERROR'
+  }
+  
+  for (let i = min; i <= max; i++) {
     sum += i
   }
   return sum

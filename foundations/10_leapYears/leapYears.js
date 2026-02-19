5a093ec4f35ab4d7d7f60a41e51acfe9b7badbf2
nilikopo <nilikopo9@gmail.com>
2026-02-19 10:37:15 +1000

    10_leapyears final solution

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,6 @@
 const leapYears = function(year) {
   if (year % 100 == 0 && year % 400 == 0) return true
-  if (year % 4 == 0) return true
+  if (year % 100 != 0 && year % 4 == 0) return true
   return false 
 };
 

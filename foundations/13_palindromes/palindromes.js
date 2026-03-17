0988fea2a8b9508b069d8939218c0e948fe983a0
nilikopo <nilikopo9@gmail.com>
2026-03-17 15:14:16 +1000

    13 | палиндром тут у меня всё плохо

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,5 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  return str.toLowerCase() == str.split('').reverse().join('').toLowerCase()
 };
 
 // Do not edit below this line

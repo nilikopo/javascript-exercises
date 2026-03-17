9d979c0f5f8be633f2d217912d16cb8126665cb1
nilikopo <nilikopo9@gmail.com>
2026-03-17 16:45:14 +1000

    16 | решение

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  return arr.reduce((oldest, current) => {
+    const getAge = person => {
+      const finish = person.yearOfDeath || new Date().getFullYear()
+      return finish - person.yearOfBirth
+    }
+    const oldestAge = getAge(oldest)
+    const currentAge = getAge(current)
 
+    return currentAge > oldestAge ? current : oldest
+  })
 };
 
 // Do not edit below this line

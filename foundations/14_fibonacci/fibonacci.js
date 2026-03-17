bd88a00cf72bc335dfde56850ccfc0dd508802c3
nilikopo <nilikopo9@gmail.com>
2026-03-17 16:19:19 +1000

    14 | фибоначи решение

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS"
+  let a = 0, b = 1
+  for (let i = 0; i < num; i++) {
+    let temp = a
+    a = b
+    b += temp
+  }
+  return a
 };
 
 // Do not edit below this line
